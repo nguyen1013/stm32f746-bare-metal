ae77c60e4b70f2b24394d35ca269be616cb9a342
Nguyen Nguyen <nasongnguyen@gmail.com>
2025-01-02 11:09:20 +0200

    timer tim1 drivers

---
+++ stm32f746-bare-metal/13-tim/Src/gpio.c
@@ -0,0 +1,12 @@
+#include "gpio.h"
+
+void set_pin_mode(GPIO_TypeDef *GPIOx, uint32_t Pin, uint32_t Mode)
+{
+	// Clear the relevant bits in the mode register
+	 //GPIOx->MODER &= ~(1U<<18);
+	 //GPIOx->MODER &= ~(1U<<19);
+	// Set the relevant bits in the mode register
+	 //GPIOx->MODER |= 1U<<19;
+	//MODIFY_REG(GPIOx->MODER, (0x3 << (POSITION_VAL(Pin) * 2U)), (Mode << (POSITION_VAL(Pin) * 2U)));
+	MODIFY_REG(GPIOx->MODER, (0x3 << (POSITION_VAL(Pin) * 2U)), (Mode << ((POSITION_VAL(Pin) * 2U))));
+}
---
+++ stm32f746-bare-metal/13-tim/Src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "stm32f7xx.h"
+#include "uart.h"
+#include "led.h"
+#include "adc.h"
+#include "systick.h"
+#include "tim.h"
+
+int main(){
+
+	int x = 0;
+
+	uart1_tx_init();
+	tim1_1hz_init();
+
+	while(1)
+	{
+		// waiting for UIF
+		while (!(TIM1->SR & TIM_SR_UIF)){}
+
+		//Clear UIF
+		TIM1->SR &= ~TIM_SR_UIF;
+
+		printf("A second passed and current second is: %d \n\r", x);
+	}
+}
